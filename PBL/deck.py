65e9dc2958eb914250c23b9e0bb84c2d832ee0e9
A.Otsuka <57290849+L4Rqm@users.noreply.github.com>
2021-07-06 16:03:32 +0900

    old_maid_main は未完成

--- PBL/deck.py
+++ PBL/deck.py
@@ -22,19 +22,23 @@ class Deck:
         print(type(self.cards))
         print(self.cards)
     
-#ババ抜きクラス　
+
+#ババ抜きクラス
 class Old_maid():
     def __init__(self,player_num):
-        self.deck = Deck(player_num)
         self.hands = Deck(player_num).make_deck()
+        self.players = [i for i in range(0, player_num)]
+        self.winplayers=[]
         self.garbage =[]
+        self.sort_hands()
+        self.delete_cards()
 
     def check_member(self):
         print(self.hands)
         print(self.garbage)
-
+    """
     #各playerのカード枚数を確認する
-    def get_num(self):
+    def check_hand(self):
         nums_list = []
         for hand in self.hands:
             num_list = []
@@ -42,8 +46,23 @@ class Old_maid():
                 num_list.append(i[1])
             nums_list.append(num_list)
 
-        return nums_list                
-    #手札をソート
+        return nums_list
+    """
+    #全プレイヤーの残りカード枚数の総和
+    def get_all_num(self):
+        num = 0
+        for hand in self.hands:
+            num = num + len(hand)
+        return num
+
+    #ゲーム終了かチェック
+    def end_game(self):
+        if self.get_all_num() == 1:
+            return 1
+        else:
+             return 0
+
+    #全手札をソート
     def sort_hands(self):
         self.list2turple()
         new_hands =[]
@@ -51,7 +70,7 @@ class Old_maid():
             new_hands.append(sorted(hand, key = lambda x:x[1]))
         self.hands = new_hands
 
-    #どこかでタプルがリストに変わってるからタプルに戻す　#あとでどこか探す
+    #どこかでタプルがリストに変わってるからタプルに戻す #あとでどこか探す
     def list2turple(self):
         news=[]
         for hand in self.hands:
@@ -62,7 +81,7 @@ class Old_maid():
             news.append(new)
         self.hands = news
 
-    #2枚組のカードを捨てる
+    #全手札に対して2枚組のカードを捨てる
     def delete_cards(self):
         flag = 0
         new_hands=[]
@@ -81,23 +100,23 @@ class Old_maid():
                     flag = 1
             self.garbage.append(garbage)
             new_hands.append(new_hand)
-        return new_hands
-    #次の人から手札取得 toplayer,fromplayer,i = int型　iは取得する手札の場所
+        #return new_hands
+        self.hands = new_hands
+
+    #次の人から手札取得 toplayer,fromplayer,i = int型 iは取得する手札の場所
     def get_card_from_player(self, toplayer,fromplayer,i):
+        print("選ばれたカード(deck.py get_card_from)" + str(self.hands[fromplayer][i]))
+        #print(self.hands[fromplayer])
         self.hands[toplayer].append(self.hands[fromplayer].pop(i))
         self.sort_hands()
         self.delete_cards()
+
+    #勝った人の確認
+    def check_win(self):
+        for i in range(len(self.hands)):
+            if self.hands[i] == []:
+                self.winplayers.append(i)
         
-
-"""
-maid = Old_maid(4)
-maid.sort_hands()
-maid.delete_cards()
-print(maid.hands)
-print("########################")
-maid.get_card_from_player(0,1,0)
-print(maid.hands)
-#print(maid.delete_cards())
-"""
-
+        #self.players = list(set(self.players) - set(self.winplayers))
+        #print(self.players)
     
